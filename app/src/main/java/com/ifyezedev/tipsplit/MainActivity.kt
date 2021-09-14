1f44745d0258fbc7aa52cec2c67ef09f370e3626
ifeadikanwa <ezeifyyy@gmail.com>
2021-09-14 19:52:23 -0400

    set up calculator layout

---
+++ app/src/main/java/com/ifyezedev/tipsplit/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.ifyezedev.tipsplit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentContainer
+import com.ifyezedev.tipsplit.calculator.CalculatorFragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_calculator)
+
+    }
+}
